ddccb786c3717a40b07c49798eb83435c3235c67
Kilian Agboton <kilian.agboton@gmail.com>
2021-04-06 23:10:37 -0400

    spring-security :: spring initializer and spring security config implementation

---
+++ src/main/java/io/kagboton/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,32 @@
+package io.kagboton.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication()
+               .withUser("john")
+               .password(passwordEncoder().encode("john")).roles("EMPLOYEE")
+               .and()
+               .withUser("mary")
+               .password(passwordEncoder().encode("mary")).roles("MANAGER")
+               .and()
+               .withUser("admin")
+               .password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
