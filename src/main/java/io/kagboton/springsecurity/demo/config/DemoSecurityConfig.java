39fdc4dcbf129d5b21f082ab2e83c77949827bb3
Kilian Agboton <kilian.agboton@gmail.com>
2021-04-08 23:53:10 -0400

    spring-security :: custom login - config security of web paths

--- src/main/java/io/kagboton/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/io/kagboton/springsecurity/demo/config/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package io.kagboton.springsecurity.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,5 +29,15 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateUser")
+                .permitAll();
     }
 }
