6fba89b0cced2461e5891182213b4d65d0b17ff1
Kilian Agboton <kilian.agboton@gmail.com>
2021-04-11 23:19:01 -0400

    spring-security :: code refactoring

--- src/main/java/io/kagboton/springsecurity/demo/controller/LoginController.java
+++ src/main/java/io/kagboton/springsecurity/demo/controller/LoginController.java
@@ -2,6 +2,7 @@ package io.kagboton.springsecurity.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
@@ -10,4 +11,6 @@ public class LoginController {
     public String showLoginForm(){
         return "fancy-login";
     }
+
+
 }
