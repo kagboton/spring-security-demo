2e5f3789efe09b74d2e30b172f2748d87a7decc7
Kilian Agboton <kilian.agboton@gmail.com>
2021-04-24 23:25:12 -0400

    spring-security :: defined datasource in spring configurarion

--- src/main/java/io/kagboton/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/io/kagboton/springsecurity/demo/config/DemoAppConfig.java
@@ -1,21 +1,38 @@
 package io.kagboton.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "io.kagboton.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig implements WebMvcConfigurer {
 
-    // define a bean for the ViewResolver
+    // set up variable to hold the properties
 
+    @Autowired
+    private Environment environment;
+
+    // set up logger for diagnostics
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+
+    // define a bean for the ViewResolver
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver =
@@ -31,4 +48,43 @@ public class DemoAppConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
     }
+
+    // define  a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource(){
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set up jdbc driver class
+        try {
+            securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw  new RuntimeException(e);
+        }
+
+        // log the connection props
+        // to make sure we are reading properties information
+        logger.info(">> jdbc.url=" + environment.getProperty("jdbc.url") );
+        logger.info(">> jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return  securityDataSource;
+    }
+
+    // helper method => read env property and convert to int
+    private int getIntProperty(String propName){
+        return Integer.parseInt(environment.getProperty(propName));
+    }
+
 }
